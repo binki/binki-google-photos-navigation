2045f8d7081b5a5925fa2e3b1863d522cb2768d9
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2024-08-11 13:22:31 -0400

    binki-google-photos-navigation-1.5

--- binki-google-photos-navigation.user.js
+++ binki-google-photos-navigation.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name binki-google-photos-navigation
-// @version 1.4
+// @version 1.5
 // @grant none
 // @author Nathan Phillip Brink (binki) (@ohnobinki)
 // @homepageURL https://github.com/binki/binki-google-photos-navigation
